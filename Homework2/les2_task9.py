fc0f6582d9c2d4e611e74a369aeb4d8a1c5b1db6
Илья <Lutiyroker@yandex.ru>
2021-10-26 11:06:16 +0300

    add homework2

--- Homework2/les2_task9.py
+++ Homework2/les2_task9.py
@@ -2,17 +2,19 @@
 Среди натуральных чисел, которые были введены, найти наибольшее по сумме цифр.
 Вывести на экран это число и сумму его цифр.
 """
-a, b = map(int, input('Введите 2 числа через пробел: ').split())
-a_cycle, b_cycle = a, b
-sum_a = 0
-sum_b = 0
-while a > 0:
-    sum_a += a % 10
-    a //= 10
-while b > 0:
-    sum_b += b % 10
-    b //= 10
-if sum_a > sum_b:
-    print(f'Большее по сумме цифр число: {a_cycle}, сумма цифр: {sum_a}')
-else:
-    print(f'Большее по сумме цифр число: {b_cycle}, сумма цифр: {sum_b}')
+num = int(input('Введите число: '))
+max_num = 0
+max_sum = 0
+while num:
+    tmp_num = num
+    tmp_sum = 0
+    while num:
+        tmp_sum += num % 10
+        num //= 10
+    if tmp_sum > max_sum:
+        max_sum = tmp_sum
+        max_num = tmp_num
+    num = int(input('Введите следующее число. Для выхода нажмите 0: '))
+print(f'Наибольшее по сумме цифр число: {max_num}, его сумма: {max_sum}')
+
+
